3b0a7dd2dd3aec22a9ca3ef3ed05641e43bd4943
DashingAdi <dashing4149@gmail.com>
2024-05-31 22:31:32 +0530

    Fix responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,10 @@ const Header = () => {
   return (
     <div>
       <header className="z-[999] relative">
-        <div
-          className="fixed flex mx-auto right-0 left-0 top-12 h-[3.25rem] w-[30rem] rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        <motion.div
+          className="fixed flex top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+          initial={{ y: -100, x: "-50%", opacity: 0 }}
+          animate={{ y: 0, x: "-50%", opacity: 1 }}
         >
           <div className="flex w-full items-center justify-evenly px-3 py-3">
             <Link className="hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300" href={"#bruh"}>Home</Link>
@@ -15,7 +17,7 @@ const Header = () => {
             <Link className="hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300" href={"#bruh"}>Projects</Link>
             <Link className="hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300" href={"#bruh"}>Contact</Link>
           </div>
-        </div>
+        </motion.div>
       </header>
     </div>
   );
